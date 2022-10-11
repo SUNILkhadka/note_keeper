4f2bf5bdd05a21cd1d1e99b1989da38a61c9a540
Doleshor <doleshorkhad@gmail.com>
2022-10-11 10:14:19 +0545

    icons_changes

---
+++ lib/views/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:note_keeper/core/app_color.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.primaryColor,
+      body: Container(),
+    );
+  }
+}
