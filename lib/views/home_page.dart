1306c9d4ece4610ae0936b98e90557d1a3434959
Doleshor khadka <doleshorkhad@gmail.com>
2022-10-20 14:39:57 +0545

    home_

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper/core/app_color.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            color: Colors.white54,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppColor.backgroundshadeblue,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    ));
   }
 }
